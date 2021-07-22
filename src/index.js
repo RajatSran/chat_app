7339de604ca77a4067f218ee603f5b18f2a1ad4e
Rajat Sran <rajatsran27@gmail.com>
2021-07-22 17:16:25 +0530

    socket.io added

---
+++ src/index.js
@@ -0,0 +1,15 @@
+const path = require('path')
+const http = require('http')
+const express = require('express')
+
+const app = express()
+const server = http.createServer(app)
+
+const port = process.env.PORT || 3000
+const publicDirectoryPath = path.join(__dirname, '../public')
+
+app.use(express.static(publicDirectoryPath))
+
+server.listen(port, () => {
+    console.log(`Server is up on port ${port}!`)
+})
