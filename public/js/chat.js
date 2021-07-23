78c08020a81f6a6001c0092b81e9b1afbd3a5db1
Rajat Sran <rajatsran27@gmail.com>
2021-07-23 22:33:22 +0530

    disabling buttons

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,29 +1,48 @@
 const socket = io()
 
+//elements
+const $messageForm = document.querySelector('#message-form')
+const $messageFormInput = $messageForm.querySelector('input')
+const $messageFormButton = $messageForm.querySelector('button')
+const $sendlocation = document.querySelector('#send-location')
+
 socket.on('message', (message) => {
     console.log(message)
 })
 
-document.querySelector('#message-form').addEventListener('submit', (e) => {
+$messageForm.addEventListener('submit', (e) => {
     e.preventDefault()
+    //disable
+    $messageFormButton.setAttribute('disabled', 'disabled')
     const message = e.target.elements.message.value
     socket.emit('sendmessage', message, (error) => {
-        if(error){
+        $messageFormButton.removeAttribute('disabled')
+        $messageFormInput.value = ''
+        $messageFormInput.focus()
+
+        //enable
+        if (error) {
             return console.log(error)
         }
         console.log('message delivered')
     })
 })
 
-document.querySelector('#send-location').addEventListener('click', () => {
+$sendlocation.addEventListener('click', () => {
     if (!navigator.geolocation) {
         return alert('geo location is not suppported')
+
     }
+    //disable
+    $sendlocation.setAttribute('disabled','disabled')
     navigator.geolocation.getCurrentPosition((position) => {
+        //enable
+        
         socket.emit('sendLocation', {
             latitude: position.coords.latitude,
             longitude: position.coords.longitude
-        },()=>{
+        }, () => {
+            $sendlocation.removeAttribute('disabled')
             console.log('location shared')
         })
     })
