59b0859862f323aa9f9e3ea8e5ddaecd8a346b83
Rajat Sran <rajatsran27@gmail.com>
2021-07-24 21:35:34 +0530

    messages are rendering on the screen

--- public/js/chat.js
+++ public/js/chat.js
@@ -5,9 +5,17 @@ const $messageForm = document.querySelector('#message-form')
 const $messageFormInput = $messageForm.querySelector('input')
 const $messageFormButton = $messageForm.querySelector('button')
 const $sendlocation = document.querySelector('#send-location')
+const $messages = document.querySelector('#messages')
+
+//templates
+const messageTemplate = document.querySelector('#message-template').innerHTML
 
 socket.on('message', (message) => {
     console.log(message)
+    const html = Mustache.render(messageTemplate,{
+        message: message
+    })
+    $messages.insertAdjacentHTML('beforeend',html)
 })
 
 $messageForm.addEventListener('submit', (e) => {
@@ -31,17 +39,16 @@ $messageForm.addEventListener('submit', (e) => {
 $sendlocation.addEventListener('click', () => {
     if (!navigator.geolocation) {
         return alert('geo location is not suppported')
-
     }
     //disable
     $sendlocation.setAttribute('disabled','disabled')
     navigator.geolocation.getCurrentPosition((position) => {
-        //enable
         
         socket.emit('sendLocation', {
             latitude: position.coords.latitude,
             longitude: position.coords.longitude
         }, () => {
+            //enable
             $sendlocation.removeAttribute('disabled')
             console.log('location shared')
         })
