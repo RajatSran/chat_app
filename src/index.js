5bde4aac0ee4ecacdf6c637a76acdd95fd3847dd
Rajat Sran <rajatsran27@gmail.com>
2021-07-30 01:31:32 +0530

    improvement added

--- src/index.js
+++ src/index.js
@@ -59,7 +59,7 @@ io.on('connection', (socket) => {
     socket.on('disconnect', () => {
         const user = removeUser(socket.id)
         if (user) {
-            io.to(user.room).emit('message', generateMessage('Admin',`A ${user.username} has left!!`))
+            io.to(user.room).emit('message', generateMessage('Admin',`${user.username} has left!!`))
             io.to(user.room).emit('roomData',{
                 room: user.room,
                 users: getUsersInRoom(user.room)
